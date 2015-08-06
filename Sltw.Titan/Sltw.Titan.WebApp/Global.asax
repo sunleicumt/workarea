53bb5e2c5e4165d0b2bcc74e612620660db54fbb
sunleicumt <sunleicumt@163.com>
2015-08-06 11:53:50 +0800

    项目搭建

---
+++ Sltw.Titan/Sltw.Titan.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sltw.Titan.WebApp.MvcApplication" Language="C#" %>
---
+++ Sltw.Titan/Sltw.Titan.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sltw.Titan.WebApp.Startup))]
+namespace Sltw.Titan.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
